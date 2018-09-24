6c28acafe686526fb647d8b6fccf78f45614c839
cobweb <codename.cobweb@gmail.com>
2018-09-24 15:01:08 +0300

    'fix'

--- app/views/layouts/admin.php
+++ app/views/layouts/admin.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="/css/main.min.css"/>
-  <?=$this->getMeta(); ?>
-</head>
-<body>
+<?php require_once 'header.php';?>
     <div class="wrapper">
         <header class="header">
             <div class="container">
@@ -26,7 +17,6 @@
             </div>
         </footer>
     </div>
-</body>
-</html>
+<?php require_once 'footer.php';?>
 
 
--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -1,52 +1,43 @@
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="/css/main.min.css"/>
-  <?=$this->getMeta(); ?>
-</head>
-<body>
+<?php require_once 'header.php';?>
     
     <div class="wrapper">
         <header class="header">
             <div class="container">
-                    <nav>
-        <ul>
-            <li>
-                <a href="/user/signup">Signup</a>
-            </li>
-            <li>
-                <a href="/user/login">Login</a>
-            </li>
-            <li>
-                <a href="/user/logout">Logout</a>
-            </li>
-        </ul>
-    </nav>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="/user/signup">Signup</a>
+                        </li>
+                        <li>
+                            <a href="/user/login">Login</a>
+                        </li>
+                        <li>
+                            <a href="/user/logout">Logout</a>
+                        </li>
+                    </ul>
+                </nav>
             </div>
         </header>
         <main class="main">
             <div class="container">
               
-    <h3>Default</h3>
-    <?=$content; ?>
+                <h3>Default</h3>
+                <?=$content; ?>
     
-    <?php if (isset($_SESSION['validate_errors'])): ?>
-    <div class="errors-validate">
-        <?=$_SESSION['validate_errors']; unset($_SESSION['validate_errors']); ?>
-    </div>
-    <?php  elseif (isset($_SESSION['validate_success'])) : ?>
-    <div class="success-validate"><?=$_SESSION['validate_success']; unset($_SESSION['validate_success']); ?></div>
-    <?php endif; ?>
-    
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <?php 
-        foreach ($scripts as $script) {
-            echo $script;
-        }
-    ?>
+                <?php if (isset($_SESSION['validate_errors'])): ?>
+                <div class="errors-validate">
+                    <?=$_SESSION['validate_errors']; unset($_SESSION['validate_errors']); ?>
+                </div>
+                <?php  elseif (isset($_SESSION['validate_success'])) : ?>
+                <div class="success-validate"><?=$_SESSION['validate_success']; unset($_SESSION['validate_success']); ?></div>
+                <?php endif; ?>
+
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+                <?php 
+                    foreach ($scripts as $script) {
+                        echo $script;
+                    }
+                ?>
             </div>
         </main>
         <footer class="footer">
@@ -56,5 +47,4 @@
         </footer>
     </div>
     
-</body>
-</html>
+<?php require_once 'footer.php';?>
