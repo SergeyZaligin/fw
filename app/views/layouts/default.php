322f804017e0755b6548779ad2b95eeb8213042f
cobweb <codename.cobweb@gmail.com>
2018-09-24 15:06:24 +0300

    'fix'

--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -32,12 +32,7 @@
                 <div class="success-validate"><?=$_SESSION['validate_success']; unset($_SESSION['validate_success']); ?></div>
                 <?php endif; ?>
 
-                <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-                <?php 
-                    foreach ($scripts as $script) {
-                        echo $script;
-                    }
-                ?>
+                
             </div>
         </main>
         <footer class="footer">
