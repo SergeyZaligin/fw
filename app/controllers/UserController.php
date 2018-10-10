71811cf03b84143078f02285a011ada66ec37a43
Sergey Zaligin <webdevjsphp@gmail.com>
2018-10-10 18:12:59 +0300

    fix

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -18,48 +18,53 @@ class UserController extends AppController
     {
         View::setMeta("Регистрация", "Регистрация", "Регистрация");
         
-        if (!empty(App::$app->request->post) && isset(App::$app->request->post)) {
-            $userModel = new User();
+        
             
+            $userModel = new User();
             if($this->isAjax()) {
+                
+                
                 $data = App::$app->request->post;
                 $userModel->load($data);
+                
                 if (!$userModel->validate($data) || !$userModel->checkUnique()){
                     $status = "=====Error validation=====";
+                    $this->loadView('ajaxSignup', $status);
+                    
+                    $userModel->getErrors();
+                    App::$app->request->session['form_data'] = $data;
+                    
                 }else{
                     $status = "=====Success validation=====";
+                    $userModel->attributes['password'] = password_hash($userModel->attributes['password'], PASSWORD_DEFAULT);
+            
+                    if ($userModel->attributes['role'] === 'admin') {
+                        $userModel->attributes['role'] = 'register';
+                    }
+
+                    if ($userModel->insert($userModel->attributes['login'], $userModel->attributes['email'], $userModel->attributes['password'], $userModel->attributes['role'] )) {
+
+                        App::$app->request->session['validate_success'] = 'Вы успешно зарегистроированы!';
+
+                    } else {
+
+                        App::$app->request->session['validate_errors'] = 'Ошибка при регистрации! ';
+
+                    }
                 }
                     
-                $this->loadView('ajaxSignup', $status);
+                
             }
             
             
             //debug($userModel->save('user'));
             //die;
             
-            if (!$userModel->validate($data) || !$userModel->checkUnique()) {
-                $userModel->getErrors();
-                App::$app->request->session['form_data'] = $data;
-                redirect();
-            }
             
-            $userModel->attributes['password'] = password_hash($userModel->attributes['password'], PASSWORD_DEFAULT);
             
-            if ($userModel->attributes['role'] === 'admin') {
-                $userModel->attributes['role'] = 'register';
-            }
             
-            if ($userModel->insert($userModel->attributes['login'], $userModel->attributes['email'], $userModel->attributes['password'], $userModel->attributes['role'] )) {
-                
-                App::$app->request->session['validate_success'] = 'Вы успешно зарегистроированы!';
-                
-            } else {
-              
-                App::$app->request->session['validate_errors'] = 'Ошибка при регистрации! ';
-                
-            }
-            redirect();
-        }
+            //redirect();
+        
     }
         
     public function loginAction() 
