cf2c852c222df516dd76218af9ff2ed240038b9b
sergey <sergey>
2018-10-15 13:02:18 +0300

    fix

--- app/views/Product/index.php
+++ app/views/Product/index.php
@@ -4,11 +4,11 @@
     </nav>
     <h1><?=$product->title; ?></h1>
     <hr>
-    <p>Отзывы к товару(0):</p>
+    <p>Отзывы к товару (0):</p>
     <?php if ($comments) : ?>
         <?php foreach ($comments as $comment) : ?>
             <h4><?=h($comment['comment_author']); ?></h4>
-            <p><?=$comment['comment_text']; ?></p>
+            <p><?=h($comment['comment_text']); ?></p>
         <?php endforeach; ?>
     <?php endif; ?>
 <?php endif; ?>
