6c4f3fe78699328bf75cc9d02ddd67c85f0cc788
sergey <sergey>
2018-10-16 11:23:34 +0300

    fix

--- app/views/Product/index.php
+++ app/views/Product/index.php
@@ -11,3 +11,20 @@
     </ul>
     <?php endif; ?>
 <?php endif; ?>
+
+<div id="form-wrapp">
+    <form class="form" method="post">
+        <div>
+            <label for="comment_author">Имя</label> <br>
+            <input type="text" name="comment_author" id="comment_author">
+        </div>
+        <div>
+            <label for="comment_text"></label><br>
+            <input type="text" name="comment_text" id="comment_text">
+        </div>
+        <input type="hidden" name="parent" value="0">
+        <div>
+            <input type="submit" name="submit" value="Отправить сообщение">
+        </div>
+    </form>
+</div>
--- app/widgets/comments/comments_tpl/comments.php
+++ app/widgets/comments/comments_tpl/comments.php
@@ -17,32 +17,3 @@
         <?php endif; ?>
     </div>
 </li>
-
-<div id="form-wrapp">
-    <form id="dialog-form" method="post">
-        <div>
-            <label for="comment_author">Имя</label> <br>
-            <input type="text" name="comment_author" id="comment_author"></div>
-        <div>
-            <label for="comment_text"></label><br>
-            <input type="text" name="comment_text" id="comment_text">
-        </div>
-        <input type="hidden" name="parent" value="0">
-        <div>
-            <input type="submit" name="submit" value="Отправить сообщение">
-        </div>
-    </form>
-</div>
-
-<script>
-    var dialog = $( "#dialog-form" ).dialog({
-      autoOpen: false,
-      height: 400,
-      width: 350,
-      modal: true,
-      
-    });
-$(".answer-comment" ).on( "click", function() {
-      dialog.dialog( "open" );
-    });
-</script>
